556f0ac5b97dffbf1c09aacf254b2612c79e11a0
bsasscer <smithbi.maricela@gmail.com>
2018-03-02 16:33:29 -0800

    add more placeholder text/images

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ProjectContainer from './projects/ProjectContainer';
+
 
 const Work = () => (
     <div className="selected-projects">
         <h2>Selected Projects</h2>
-        <ProjectContainer />
+
     </div>
 );
 
