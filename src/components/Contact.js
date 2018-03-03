b81d02b7be5180731382e354a936572c14efb8f9
bsasscer <smithbi.maricela@gmail.com>
2018-03-02 16:27:06 -0800

    add basic stuff to contact page

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Contact = (props) => (
+    <div className="main-content">
+        <h2>{ props.title }</h2>
+        <p>Let's connect!</p>
+    </div>
+);
+
+export default Contact;
