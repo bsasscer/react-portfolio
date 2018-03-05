3fb7b98c8de14fc6d265d10b6447a23eb04dc28f
bsasscer <smithbi.maricela@gmail.com>
2018-03-05 14:00:28 -0800

    add placeholder portfolio data

---
+++ src/portfolio-data.js
@@ -0,0 +1,31 @@
+const portfolio = {
+    entry1: {
+        name: "Work Sample 1",
+        image: "/images/daria-nepriakhina-474036-unsplash.jpg",
+        desc:
+            "Kale chips chicharrones tumblr single-origin coffee slow-carb umami intelligentsia authentic tacos."
+    },
+
+    entry2: {
+        name: "Work Sample 2",
+        image: "/images/daria-nepriakhina-474036-unsplash.jpg",
+        desc:
+            "Bitters cornhole pabst marfa letterpress plaid chia affogato godard pug edison bulb austin selvage echo park next level."
+    },
+
+    entry3: {
+        name: "Work Sample 3",
+        image: "/images/daria-nepriakhina-474036-unsplash.jpg",
+        desc:
+            "La croix vaporware meggings, organic bespoke poutine vape air plant portland. IPhone celiac mixtape, letterpress pop-up chia vinyl kale chips."
+    },
+
+    entry4: {
+        name: "Work Sample 4",
+        image: "/images/daria-nepriakhina-474036-unsplash.jpg",
+        desc:
+            "Microdosing sriracha poke quinoa readymade. Vinyl shaman PBR&B cray tote bag prism."
+    }
+};
+
+export default portfolio;
