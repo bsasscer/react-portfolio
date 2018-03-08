e2458b312b59a51944da65ffcdfc1d77737c64ff
bsasscer <smithbi.maricela@gmail.com>
2018-03-08 14:50:44 -0800

    add basic contact form and display it on Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ContactForm from './ContactForm';
 
-const Contact = (props) => (
-    <div className="main-content">
-        <h2>{ props.title }</h2>
-        <p>Let's connect!</p>
-    </div>
-);
+class Contact extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Contact Me</h1>
+                <ContactForm />
+            </div>
+        );
+    }
+}
 
 export default Contact;
---
+++ src/components/ContactForm.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class ContactForm extends React.Component {
+
+    handleSubmit = event => {
+        event.preventDefault()
+        const value = event.target.elements.firstName.value;
+        console.log(value);
+    };
+
+    render() {
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    First Name
+                    <input
+                        type="text"
+                        name="firstName"
+                    />
+                </label>
+                <label>
+                    Last Name
+                    <input
+                        type="text"
+                        name="lastName"
+                    />
+                </label>
+                <label>
+                    Email
+                    <input
+                        type="text"
+                        name="email"
+                    />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        );
+    }
+}
+
+export default ContactForm;
