839bef2061a5581836da524234401fb5598c281a
bsasscer <smithbi.maricela@gmail.com>
2018-03-12 18:20:37 -0700

    add skills/services to about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,28 @@ class About extends React.Component {
     render() {
         return (
             <div>
+                <div className="skills-donut-chart"></div>
                 <div className="item portrait"></div>
+                <div className="skills-key">
+                    <div className="skill-type">
+                        <p>
+                            <span className="skill-color skill-web"></span>
+                            Web Development
+                        </p>
+                    </div>
+                    <div className="skill-type">
+                        <p>
+                            <span className="skill-color skill-ui"></span>
+                            UX/UI
+                        </p>
+                    </div>
+                    <div className="skill-type">
+                        <p>
+                            <span className="skill-color skill-mobile"></span>
+                            Mobile Development
+                        </p>
+                    </div>
+                </div>
                 <h2 className="my-name">Hi, I'm Josephine Shmoe.</h2>
                 <div className="about wrapper multicol">
                     <p>For 3+ years I have been using my creativity and problem-solving skills to translate user feedback into scalable solutions. The first ascent, made in June from the Paris Observatory, though a lofty one, was attended with so much danger and confusion as to be barren of results. The departure, owing to stormy weather, was hurried and illordered, so that the velocity in rising was excessive, the net constricted the rapidly-swelling globe, and the volumes of out-rushing gas half-suffocated the voyagers. Then a large rent occurred, which caused an alarmingly rapid fall, and the two philosophers were reduced to the necessity of flinging away all they possessed, their instruments only excepted. The landing, in a vineyard, was happily not attended with disaster, and within a month the same two colleagues attempted a second aerial excursion, again in wet weather.
